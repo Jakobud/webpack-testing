b17318f102d4ef806ee00dc386c65ee0d7e965d5
Jake Wilson <jakewilson@liveareacx.com>
2021-03-25 23:45:24 -0600

    Updated code

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,22 @@
 
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   output: {
     path: path.resolve('./dist'),
-    filename: '[name].css'
+    filename: '[name].js'
   },
   stats: {
     children: true
   },
   name: 'scss',
   entry: {
-    file1: path.resolve('./test1.scss'),
-    file2: path.resolve('./test2.scss')
+    file1: path.resolve('./src/test1.scss'),
+    file2: path.resolve('./src/test2.scss')
   },
   module: {
     rules: [
@@ -36,6 +38,19 @@ module.exports = {
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin()
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: [
+        path.join(process.cwd(), 'dist', '**', '*.js'),
+      ]
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+
+    new StylelintPlugin({
+      configFile: path.join(process.cwd(), 'stylelint.config.js'),
+      context: path.join(process.cwd(), 'src')
+    })
   ]
 }
